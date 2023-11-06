12e44bf568a3b7bc923fadb2ff1a312056780a63
sneelika <sayuruneelika@gmail.com>
2023-11-06 11:45:47 +0530

    "test"

--- todo backend/lambda.mjs
+++
@@ -1,9 +0,0 @@
-// "use strict";
-// const app = require("./src/app");
-// const serverless = require("serverless-http");
-// module.exports.hello = serverless(app);
-"use strict";
-import app from "./src/app.ts";
-import serverless from "serverless-http";
-
-export const hello = serverless(app);
